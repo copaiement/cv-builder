b7bde9cb0a1221e62c8b602178a9a69a98cab91d
copaiement <cole.paiement@gmail.com>
2023-09-20 17:15:25 -0700

    Added notes for tomorrow

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,7 +1,22 @@
+import { useState } from "react";
 import { experience, addExperience } from "./Data"
 import { InputModule } from "./Components";
 
 export function Experience() {
+  const [expList, setExpList] = useState(experience)
+
+  // move function to add a new list here
+
+  // remove original list and create in Data with a function (and and ID)
+
+  // update state with button
+
+  // pass through function for button
+
+  // change single InputModule call to a map function
+
+  // move buttons creation to after inputs instead of inside 
+
   return (
     <InputModule 
       title='Experience'
